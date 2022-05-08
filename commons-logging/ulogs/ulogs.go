0817b22dc26af6bcf172f3ebdf014d2cae367a12
zhangfucheng <zhfchdev@gmail.com>
2022-05-09 00:31:00 +0800

    compatible with go version below 1.17

--- commons-logging/ulogs/ulogs.go
+++ commons-logging/ulogs/ulogs.go
@@ -2,22 +2,22 @@ package ulogs
 
 import "log"
 
-func Debug(format string, v ...any) {
+func Debug(format string, v ...interface{}) {
 	log.Printf(format, v...)
 }
 
-func Warn(format string, v ...any) {
+func Warn(format string, v ...interface{}) {
 	log.Printf(format, v...)
 }
 
-func Info(format string, v ...any) {
+func Info(format string, v ...interface{}) {
 	log.Printf(format, v...)
 }
 
-func Error(format string, v ...any) {
+func Error(format string, v ...interface{}) {
 	log.Printf(format, v...)
 }
 
-func Panic(format string, v ...any) {
+func Panic(format string, v ...interface{}) {
 	log.Panicf(format, v...)
 }
