cea2fea6838bec842123948c114ab349606058c3
Fucheng <zhfchdev@gmail.com>
2023-08-23 20:44:42 +0800

    add ToInt32 to uconv

--- commons-lang/uconv/int_conv.go
+++ commons-lang/uconv/int_conv.go
@@ -1,6 +1,9 @@
 package uconv
 
-import "math"
+import (
+	"github.com/duke-git/lancet/v2/convertor"
+	"math"
+)
 
 func IntPtr(i int) *int {
 	return &i
@@ -8,4 +11,13 @@ func IntPtr(i int) *int {
 
 func Uint64ToInt64(n uint64) int64 {
 	return int64(n & math.MaxInt64)
+}
+
+var (
+	ToInt = convertor.ToInt
+)
+
+func ToInt32(n any) (int32, error) {
+	i, err := ToInt(n)
+	return int32(i & 0xffff), err
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/congta/ugo-commons
 
 go 1.20
 
-require github.com/bytedance/gopkg v0.0.0-20230728082804-614d0af6619b // indirect
+require (
+	github.com/bytedance/gopkg v0.0.0-20230728082804-614d0af6619b
+	github.com/duke-git/lancet/v2 v2.2.5
+)
+
+require golang.org/x/text v0.9.0 // indirect
